ea58b2b68c57c271b5574e8fc2fc0c0717b233c0
Jame <enzosuname@gmail.com>
2022-04-13 09:29:38 -0400

    got the basic structure down, trying to place textures

--- main.py
+++ main.py
@@ -5,20 +5,12 @@ from settings import *
 import sys
 import math
 
-# Initialize Pygame
-pg.init
-
-# Set Base Screen
-screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pg.display.set_caption("Raycasting")
-clock = pg.time.Clock()
-
 # draw the map
 def draw_map():
     for row in range(8):
-        #loop over map columns
+        # loop over map columns
         for col in range(8):
-            #calculate square index
+            # calculate square index
             square = row * MAP_SIZE + col
 
             # draw map in the game window
@@ -63,12 +55,12 @@ def cast_rays():
             square = row * MAP_SIZE + col
 
             if MAP[square] == '#':
-                pygame.draw.rect(screen, (0, 255, 0), (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE - 2, TILE_SIZE - 2))
+                #pygame.draw.rect(screen, (0, 255, 0), (col * TILE_SIZE, row * TILE_SIZE, TILE_SIZE - 2, TILE_SIZE - 2))
 
 
                 #draw casted ray
-                pg.draw.line(screen, (0, 255, 0), (player_x, player_y),
-                             (target_x, target_y))
+                #pg.draw.line(screen, (0, 255, 0), (player_x, player_y),
+                             #(target_x, target_y))
 
                 color = 255/(1 + depth * depth * 0.0001)
                 depth *= math.cos(player_angle - start_angle)
@@ -80,7 +72,7 @@ def cast_rays():
 
                 # draw 3d projection
                 pg.draw.rect(screen, (color, color, color),
-                             (SCREEN_HEIGHT + ray * SCALE,
+                             (ray * SCALE,
                               (SCREEN_HEIGHT / 2) - wall_height / 2,
                               SCALE, wall_height))
 
@@ -99,26 +91,42 @@ while running:
     if keys[pg.K_LEFT]: player_angle -= 0.1
     if keys[pg.K_RIGHT]: player_angle += 0.1
     if keys[pg.K_UP]:
+        forward = True
         player_x += -math.sin(player_angle) * 3
         player_y += math.cos(player_angle) * 3
     if keys[pg.K_DOWN]:
+        forward = False
         player_x -= -math.sin(player_angle) * 3
         player_y -= math.cos(player_angle) * 3
 
+    col = int(player_x / TILE_SIZE)
+    row = int(player_y / TILE_SIZE)
+
+    square = row * MAP_SIZE + col
+
+    if MAP[square] == '#':
+        if forward:
+            player_x -= -math.sin(player_angle) * 3
+            player_y -= math.cos(player_angle) * 3
+        else:
+            player_x += -math.sin(player_angle) * 3
+            player_y += math.cos(player_angle) * 3
+
+
 
     # background
-    pg.draw.rect(screen, (0, 0, 0), (0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT))
-
-    # update 3d background
-    pg.draw.rect(screen, (100, 100, 100), (480, SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
-    pg.draw.rect(screen, (200, 200, 200), (480, -SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
+    #pg.draw.rect(screen, (0, 0, 0), (0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT))
 
     # draw 2D map
-    draw_map()
+    #draw_map()
+
+    pg.draw.rect(screen, (100, 100, 100), (0, SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
+    pg.draw.rect(screen, (200, 200, 200), (0, -SCREEN_HEIGHT / 2, SCREEN_HEIGHT, SCREEN_HEIGHT))
 
     # apply raycasting
     cast_rays()
 
+    clock.tick(30)
+
     pg.display.flip()
 
-    clock.tick(30)
--- settings.py
+++ settings.py
@@ -4,30 +4,45 @@ import sys
 import math
 
 # global constants
-SCREEN_HEIGHT = 480
-SCREEN_WIDTH = SCREEN_HEIGHT * 2
+SCREEN_HEIGHT = 480 * 2
+SCREEN_WIDTH = SCREEN_HEIGHT
 MAP_SIZE = 8
-TILE_SIZE = int((SCREEN_WIDTH / 2) / MAP_SIZE)
+TILE_SIZE = int(SCREEN_HEIGHT / MAP_SIZE)
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
 CASTED_RAYS = 120
 STEP_ANGLE = FOV / CASTED_RAYS
 MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
-SCALE = (SCREEN_WIDTH / 2) / CASTED_RAYS
+SCALE = SCREEN_HEIGHT / CASTED_RAYS
 
 # global variables
 player_x = SCREEN_HEIGHT - SCREEN_HEIGHT/2
 player_y = SCREEN_HEIGHT - SCREEN_HEIGHT/2
 player_angle = math.pi
 
+pg.init()
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pg.display.set_caption("Raycasting")
+clock = pg.time.Clock()
+
+#background = pygame.image.load('assets/background.png').convert()
+walls = pygame.image.load('assets/walls.png').convert()
+textures = {
+        'S': walls.subsurface(0, 0, 64, 64),
+        'T': walls.subsurface(0, 0, 64, 64),
+        'O': walls.subsurface(0, 0, 64, 64),
+        'D': walls.subsurface(2 * 64, 2 * 64, 64, 64),
+        'W': walls.subsurface(4 * 64, 3 * 64, 64, 64),
+        'X': walls.subsurface(0, 2 * 64, 64, 64)}
+
 # map
-MAP = (
+MAP = list(
     '########'
     '#      #'
+    '#  ### #'
     '#    # #'
     '#      #'
-    '#      #'
     '#  #   #'
-    '#      #'
+    '#  #   #'
     '########'
 )
