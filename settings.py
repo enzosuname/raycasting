dd78e3c0cdb6752a944be734c07f999a9c24571a
Jame <enzosuname@gmail.com>
2022-05-02 09:40:10 -0400

    Add having multiple textures up at once, figure out if there is lag fix.

--- settings.py
+++ settings.py
@@ -10,7 +10,7 @@ MAP_SIZE = 8
 TILE_SIZE = int(SCREEN_HEIGHT / MAP_SIZE)
 FOV = math.pi / 3
 HALF_FOV = FOV / 2
-CASTED_RAYS = 100
+CASTED_RAYS = 80
 STEP_ANGLE = FOV / CASTED_RAYS
 MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
 SCALE = SCREEN_HEIGHT / CASTED_RAYS
@@ -28,16 +28,6 @@ pg.init()
 screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pg.display.set_caption("Raycasting")
 clock = pg.time.Clock()
-
-#background = pygame.image.load('assets/background.png').convert()
-# walls = pygame.image.load('assets/walls.png').convert()
-# textures = {
-#         'S': walls.subsurface(0, 0, 64, 64),
-#         'T': walls.subsurface(0, 0, 64, 64),
-#         'O': walls.subsurface(0, 0, 64, 64),
-#         'D': walls.subsurface(2 * 64, 2 * 64, 64, 64),
-#         'W': walls.subsurface(4 * 64, 3 * 64, 64, 64),
-#         'X': walls.subsurface(0, 2 * 64, 64, 64)}
 
 # map
 MAP = (
